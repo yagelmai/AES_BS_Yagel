931071bfec8bf9181b774dd2c8a86b4cadaa9ad3
yagelmai <yagel.maimon@intel.com>
2021-12-05 22:53:59 +0200

    need to fix cipher and done

--- check_tests.sh
+++ check_tests.sh
@@ -1,4 +1,4 @@
-gcc -no-pie test_KM.c aux_tests.c aux_code.o students_code.S -o test_KM.out
+gcc test_KM.c aux_tests.c aux_code.o students_code.S -o test_KM.out
 if [ -f "test_KM.out" ]; then
 	timeout 20s ./test_KM.out < test_KM_1 > KM_out
 	if [ $? -eq 0 ]; then
@@ -9,7 +9,7 @@ if [ -f "test_KM.out" ]; then
 	fi
 fi
 
-gcc -no-pie test_BS.c aux_tests.c aux_code.o students_code.S -o test_BS.out
+gcc test_BS.c aux_tests.c aux_code.o students_code.S -o test_BS.out
 if [ -f "test_KM.out" ]; then
 	timeout 20s ./test_BS.out < test_BS_1 > BS_out
 	if [ $? -eq 0 ]; then
@@ -20,7 +20,7 @@ if [ -f "test_KM.out" ]; then
 	fi
 fi
 
-gcc -no-pie test_SR.c aux_tests.c aux_code.o students_code.S -o test_SR.out
+gcc test_SR.c aux_tests.c aux_code.o students_code.S -o test_SR.out
 if [ -f "test_KM.out" ]; then
 	timeout 20s ./test_SR.out < test_SR_1 > SR_out
 	if [ $? -eq 0 ]; then
@@ -31,7 +31,7 @@ if [ -f "test_KM.out" ]; then
 	fi
 fi
  
-gcc -no-pie test_cipher.c aux_tests.c aux_code.o students_code.S -o test_cipher.out
+gcc test_cipher.c aux_tests.c aux_code.o students_code.S -o test_cipher.out
 if [ -f "test_KM.out" ]; then
 	timeout 20s ./test_cipher.out < test_cipher_1 > cipher_out
 	if [ $? -eq 0 ]; then
